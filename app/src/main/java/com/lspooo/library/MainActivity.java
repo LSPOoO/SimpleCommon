d1d359a082b237affe9e3776fc2d0c7fb733f217
LSPOoO <lsp929488@163.com>
2025-03-13 23:22:49 +0800

    清楚无效代码

--- app/src/main/java/com/lspooo/library/MainActivity.java
+++ app/src/main/java/com/lspooo/library/MainActivity.java
@@ -1,38 +1,22 @@
 package com.lspooo.library;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        EdgeToEdge.enable(this);
-        setDisplayHomeAsUp(false);
-        setToolbarTitle(getString(R.string.app_name));
-        //Launcher禁止侧滑
-        setSwipeBackEnable(false);
-
+        setContentView(R.layout.activity_main);
         findViewById(R.id.btn_material_design).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, MaterialDesignActivity.class);
-                startActivity(intent);
+
             }
         });
     }
 
-    @Override
-    protected int getLayoutId() {
-        return R.layout.activity_main;
-    }
-
-    @Override
-    protected boolean hasToolBar() {
-        return true;
-    }
 }
